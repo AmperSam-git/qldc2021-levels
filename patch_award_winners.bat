b5350efdc75b89b8530aead1736d20c1c7c42f1c
AmperSam <81581391+ampersam-smw@users.noreply.github.com>
2021-10-21 01:03:52 -0230

    Update patch_award_winners.bat
    update script

--- patch_award_winners.bat
+++ patch_award_winners.bat
@@ -1,8 +1,14 @@
 @echo off
 
 set ROMFILE="smw.sfc"
-
-for /f "tokens=*" %%a in (award-winners.txt) do (
-	.\flips.exe --apply bps\%%a %ROMFILE% smc\QLDC2021_winners\%%a.smc
+if exist smc\ (echo) else (mkdir smc)
+if exist %ROMFILE% (
+	for /f "tokens=*" %%a in (award-winners.txt) do (
+		.\flips.exe --apply bps\%%a.bps %ROMFILE% smc\%%a.smc
+	)
+	pause
+) else (
+    rem file doesn't exist
+    echo -- Couldn't find %ROMFILE% to apply patches to, please include your original version of SMW renamed to that.
+    pause
 )
-
